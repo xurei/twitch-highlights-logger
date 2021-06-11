5651337b3795df337ecb50b0630c46ab66dab3ce
xurei <olivier.bourdoux@gmail.com>
2021-06-11 14:35:26 +0200

    Load app when DOMContentLoaded is already fired

--- src/webapp/webapp.js
+++ src/webapp/webapp.js
@@ -6,13 +6,24 @@ import MainView from './main_view';
 window.global = window; //eslint-disable-line no-undef
 const document = window.document; //eslint-disable-line no-undef
 
-window.addEventListener('DOMContentLoaded', function() { //eslint-disable-line no-undef
+function initApp() { //eslint-disable-line no-undef
     ReactDOM.render(
-        <MainView/>,
-        document.getElementById('content'),
+      <MainView/>,
+      document.getElementById('content'),
     );
     global.postMessage({ action: 'webapp_ready', data: null }, '*');
     setTimeout(() => {
         document.getElementById('loader').style.opacity = '0.0';
     }, 700);
-});
+}
+
+if( document.readyState !== 'loading' ) {
+    console.log( 'document is already ready' );
+    initApp();
+}
+else {
+    console.log( 'document not ready, waiting for it to load...' );
+    document.addEventListener('DOMContentLoaded', initApp);
+}
+
+//window.addEventListener('DOMContentLoaded', initApp);

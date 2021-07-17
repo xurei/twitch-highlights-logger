ffbc7aafca67a4361b267a00eb46d4119724ec5c
xurei <olivier.bourdoux@gmail.com>
2021-07-17 23:33:09 +0200

    Chat integration

---
+++ src/webapp/seconds-to-time.js
@@ -0,0 +1,11 @@
+export function secondsToTime(t) {
+  t = Math.floor(t);
+  const seconds = (t%60);
+  t -= seconds;
+  t /= 60;
+  const minutes = (t%60);
+  t -= minutes;
+  t /= 60;
+  const hours = t;
+  return `${hours}:${(minutes < 10 ? '0' : '') + minutes}:${(seconds < 10 ? '0' : '') + seconds}`;
+}
